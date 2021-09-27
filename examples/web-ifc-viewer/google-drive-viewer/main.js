47c76a79ac99ca006bfebb7d40de37addcbd02ee
Antonio <antoniogviegas@hotmail.com>
2021-09-28 00:35:22 +0200

    Add google drive example

---
+++ examples/web-ifc-viewer/google-drive-viewer/main.js
@@ -0,0 +1,50 @@
+import {IfcViewerAPI} from 'web-ifc-viewer';
+import {Color} from 'three'
+
+// Get div container where 3d scene is rendered
+const container = document.getElementById('viewer-container');
+
+// Initialize IFC.js API and add it as global variable
+const viewer = new IfcViewerAPI({container, backgroundColor: new Color(0xffffff)});
+viewer.IFC.applyWebIfcConfig({ COORDINATE_TO_ORIGIN: true, USE_FAST_BOOLS: true });
+window.webIfcAPI = viewer;
+
+// Set up scene
+viewer.addAxes();
+viewer.addGrid(50, 50);
+viewer.IFC.setWasmPath('wasm/');
+viewer.clipper.active = true;
+let dimensionsActive = false;
+
+// Add basic input logic
+const handleKeyDown = (event) => {
+    if (event.code === 'KeyE') {
+        dimensionsActive = !dimensionsActive;
+        viewer.dimensions.active = dimensionsActive;
+        viewer.dimensions.previewActive = dimensionsActive;
+        viewer.IFC.unPrepickIfcItems();
+        window.onmousemove = dimensionsActive ? null : viewer.IFC.prePickIfcItem;
+    }
+    if (event.code === 'KeyD') {
+        viewer.dimensions.create();
+    }
+    if (event.code === 'KeyG') {
+        viewer.clipper.createPlane();
+    }
+    if (event.code === 'Delete') {
+        viewer.dimensions.deleteAll();
+        viewer.clipper.deletePlane();
+        viewer.IFC.unpickIfcItems();
+    }
+};
+window.onkeydown = handleKeyDown;
+
+// Highlight items when hovering over them
+window.onmousemove = viewer.IFC.prePickIfcItem;
+
+// Select items and log properties
+window.ondblclick = async () => {
+    const item = await viewer.IFC.pickIfcItem(true);
+    if(item.modelID === undefined || item.id === undefined ) return;
+    console.log(await viewer.IFC.getProperties(item.modelID, item.id, true));
+}
---
+++ examples/web-ifc-viewer/google-drive-viewer/utils/google-drive-setup.js
@@ -0,0 +1,122 @@
+// SOURCE: https://developers.google.com/drive/api/v3/quickstart/js
+
+// Client ID and API key from the Developer Console
+// Beware: I have my own API keys. If you want to access to your google drive projects, click on the link above and
+// follow the guide to get yours. Then, simply copy the values here.
+let CLIENT_ID = '';
+let API_KEY = '';
+
+async function getCredentials() {
+    const file = await fetch('keys.json');
+    if (!file.ok) throw new Error('Credentials not found! Have you created your own credentials?');
+    const json = await file.json();
+    if (typeof json !== 'object') throw new Error('Credentials could not be parsed');
+    CLIENT_ID = json.CLIENT_ID;
+    API_KEY = json.API_KEY;
+}
+
+// Array of API discovery doc URLs for APIs used by the quickstart
+const DISCOVERY_DOCS = ["https://www.googleapis.com/discovery/v1/apis/drive/v3/rest"];
+
+// Authorization scopes required by the API; multiple scopes can be
+// included, separated by spaces.
+const SCOPES = 'https://www.googleapis.com/auth/drive';
+
+// Get html elements
+const authorizeButton = document.getElementById('authorize_button');
+const buttonText = document.getElementById('authorize_button-text');
+const fileName = document.getElementById('file-name');
+
+/**
+ *  On load, called to load the auth2 library and API client library.
+ */
+async function handleClientLoad() {
+    await getCredentials();
+    gapi.load('client:auth2', initClient);
+}
+
+/**
+ *  Initializes the API client library and sets up sign-in state
+ *  listeners.
+ */
+function initClient() {
+    const result = authenticateUser();
+    result.then(setupAuthEvents, logAuthErrors);
+}
+
+function authenticateUser() {
+    return gapi.client.init({
+        apiKey: API_KEY,
+        clientId: CLIENT_ID,
+        discoveryDocs: DISCOVERY_DOCS,
+        scope: SCOPES
+    });
+}
+
+function setupAuthEvents() {
+    // Listen for sign-in state changes.
+    gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
+
+    // Handle the initial sign-in state.
+    updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
+    authorizeButton.onclick = handleAuthClick;
+}
+
+function logAuthErrors(error) {
+    console.log(JSON.stringify(error, null, 2));
+}
+
+/**
+ *  Called when the signed in status changes, to update the UI
+ *  appropriately. After a sign-in, the API is called.
+ */
+function updateSigninStatus(isSignedIn) {
+    if (isSignedIn) {
+        const {id, name} = getIdAndName();
+        fileName.innerText = `Name: ${name}`;
+        if (id) downloadFile(id);
+        else console.log('No ID was providen as URL parameter!');
+    }
+}
+
+/**
+ *  Sign in the user upon button click.
+ */
+function handleAuthClick(event) {
+    authorizeButton.onclick = handleSignoutClick;
+    buttonText.innerText = 'Sign out';
+    gapi.auth2.getAuthInstance().signIn();
+}
+
+/**
+ *  Sign out the user upon button click.
+ */
+function handleSignoutClick(event) {
+    authorizeButton.onclick = handleAuthClick;
+    buttonText.innerText = 'Sign in';
+    gapi.auth2.getAuthInstance().signOut();
+}
+
+function downloadFile(fileId) {
+    gapi.client.drive.files.get({
+        fileId: fileId,
+        alt: 'media'
+    }).then(function (response) {
+        loadIfcModel(response.body);
+    }, function (error) {
+        console.error(error)
+    })
+}
+
+function getIdAndName() {
+    const urlParams = new URLSearchParams(window.location.search);
+    const id = urlParams.get("id");
+    const name = urlParams.get("name");
+    return {id, name};
+}
+
+function loadIfcModel(text) {
+    const blob = new Blob([text], {type: 'text/plain'});
+    const file = new File([blob], "ifcModel");
+    window.webIfcAPI.IFC.loadIfc(file);
+}
