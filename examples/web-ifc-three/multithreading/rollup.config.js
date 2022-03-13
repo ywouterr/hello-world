0035a34a90477dc2c4d20bd9f68b754a57c6c7a0
Ansoni <antoniogviegas@hotmail.com>
2022-03-13 01:23:21 +0100

    Update multithreading example folder name

--- examples/web-ifc-three/multithreading/rollup.config.js
+++ examples/web-ifc-three/multithreading/rollup.config.js
@@ -1,11 +1,11 @@
 import resolve from '@rollup/plugin-node-resolve';
 
 export default {
-  input: 'examples/web-ifc-three/advanced-loading/app.js',
+  input: 'examples/web-ifc-three/multithreading/app.js',
   output: [
     {
       format: 'esm',
-      file: 'examples/web-ifc-three/advanced-loading/bundle.js'
+      file: 'examples/web-ifc-three/multithreading/bundle.js'
     },
   ],
   plugins: [
