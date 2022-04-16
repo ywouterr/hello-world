dbb9f9788083330934b7eebbfa1c383522836cae
Ansoni <antoniogviegas@hotmail.com>
2022-04-16 09:23:34 +0200

    First version of interior voxels, but would be better to traverse by Y

--- examples/web-ifc-three/element-orientation/bundle.js
+++ examples/web-ifc-three/element-orientation/bundle.js
@@ -82337,8 +82337,8 @@ const scene = new Scene();
 
 //Object to store the size of the viewport
 const size = {
-	width: window.innerWidth,
-	height: window.innerHeight,
+    width: window.innerWidth,
+    height: window.innerHeight,
 };
 
 //Creates the camera (point of view of the user)
@@ -82361,7 +82361,7 @@ scene.add(directionalLight.target);
 
 //Sets up the renderer, fetching the canvas of the HTML
 const threeCanvas = document.getElementById('three-canvas');
-const renderer = new WebGLRenderer({ canvas: threeCanvas, alpha: true });
+const renderer = new WebGLRenderer({canvas: threeCanvas, alpha: true});
 renderer.setSize(size.width, size.height);
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 
@@ -82381,255 +82381,231 @@ controls.target.set(-2, 0, 0);
 
 //Animation loop
 const animate = () => {
-	controls.update();
-	renderer.render(scene, camera);
-	requestAnimationFrame(animate);
+    controls.update();
+    renderer.render(scene, camera);
+    requestAnimationFrame(animate);
 };
 
 animate();
 
 //Adjust the viewport to the size of the browser
 window.addEventListener('resize', () => {
-	(size.width = window.innerWidth), (size.height = window.innerHeight);
-	camera.aspect = size.width / size.height;
-	camera.updateProjectionMatrix();
-	renderer.setSize(size.width, size.height);
+    (size.width = window.innerWidth), (size.height = window.innerHeight);
+    camera.aspect = size.width / size.height;
+    camera.updateProjectionMatrix();
+    renderer.setSize(size.width, size.height);
 });
 
 // Setup raycasting
-const raycaster = new Raycaster();
-const resolution = 0.5;
-const raycastOffset = 1;
-const storedPositions = {};
-const storedFilledPositions = {};
 
-const material = new MeshLambertMaterial({ color: 0x00ff00, transparent: true, opacity: 0.2 });
-const materialFill = new MeshLambertMaterial({ color: 0xff0000, transparent: true, opacity: 0.2 });
+const raycaster = new Raycaster();
+const resolution = 1;
+const raycastOffset = 1;
+
+const voxels = {
+    count: 0,
+    matrices: {}
+};
+
+const innerVoxels = {
+    count: 0,
+    matrices: {}
+};
+
+new MeshLambertMaterial({color: 0x00ff00, transparent: true, opacity: 0.2});
+const innerMaterial = new MeshLambertMaterial({color: 0xff0000, transparent: true, opacity: 0.2});
 const geometry = new BoxGeometry(resolution * 0.8, resolution * 0.8, resolution * 0.8);
 
-const voxelsMatrices = [];
-const voxelsFillMatrices = [];
 const rotation = new Quaternion();
 const scale = new Vector3(1, 1, 1);
 
 const ifcLoader = new IFCLoader();
 
 async function loadIFC() {
-	await ifcLoader.ifcManager.setWasmPath('../../../');
-	await ifcLoader.ifcManager.applyWebIfcConfig({
-		USE_FAST_BOOLS: true,
-		COORDINATE_TO_ORIGIN: true,
-	});
+    await ifcLoader.ifcManager.setWasmPath('../../../');
+    await ifcLoader.ifcManager.applyWebIfcConfig({
+        USE_FAST_BOOLS: true,
+        COORDINATE_TO_ORIGIN: true,
+    });
 
-	// Sets up optimized picking
-	await ifcLoader.ifcManager.setupThreeMeshBVH(
-		computeBoundsTree,
-		disposeBoundsTree,
-		acceleratedRaycast);
+    // Sets up optimized picking
+    await ifcLoader.ifcManager.setupThreeMeshBVH(
+        computeBoundsTree,
+        disposeBoundsTree,
+        acceleratedRaycast);
 
-	const model = await ifcLoader.loadAsync('../../../IFC/01.ifc');
-	model.geometry.computeVertexNormals();
-	scene.add(model);
-	console.log(model);
+    const model = await ifcLoader.loadAsync('../../../IFC/02.ifc');
+    scene.add(model);
+    model.visible = false;
+    console.log(model);
 
-	const subset = await getSubsetToVoxelize();
+    // model.material.forEach(mat => {
+    // 	mat.opacity = 0.2;
+    // 	mat.transparent = true;
+    // });
+    // model.visible = false;
 
-	// model.material.forEach(mat => {
-	// 	mat.opacity = 0.2;
-	// 	mat.transparent = true;
-	// });
-	// model.visible = false;
+    // Voxelize
+    await renderOuterVoxels(model);
+    renderInnerVoxels();
+    showVoxels();
 
-	// Voxelize
-	const min = model.geometry.boundingBox.min;
-	const max = model.geometry.boundingBox.max;
+    // Create a subset with all the walls
+    // For each wall >
+    //     take the biggest triangle and check a point with some offset in the direction of the normal
+    //     voxelize the point
+    //     if the voxel exists in the stored voxels, the orientation is the opposite: if not, that normal is the orientation
+    //     for windows and doors: check relation objects and the orientation is equivalent to the parent wall
 
-	renderVoxels(min, max, subset, 'x', 'y', 'z');
-	renderVoxels(min, max, subset, 'z', 'y', 'x');
-	renderVoxels(min, max, subset, 'x', 'z', 'y');
-	showVoxels();
 
-	// Fill with voxels the interior
-	// Create a subset with all the walls
-	// For each wall >
-	//     take the biggest triangle and check a point with some offset in the direction of the normal
-	//     voxelize the point
-	//     if the voxel exists in the stored voxels, the orientation is the opposite: if not, that normal is the orientation
-	//     for windows and doors: check relation objects and the orientation is equivalent to the parent wall
+    console.log(voxels);
+    console.log(innerVoxels);
 
-	const offsetMin = new Vector3(min.x - 2 * resolution, min.y - 2 * resolution, min.z - 2 * resolution);
-	const offsetMax = new Vector3(max.x + 2 * resolution, max.y + 2 * resolution, max.z + 2 * resolution);
+}
 
-	const voxelizedMin = getVoxelizedPosition({point: offsetMin});
-	const voxelizedMax = getVoxelizedPosition({point: offsetMax});
+async function renderOuterVoxels(model) {
+    const subset = await getSubsetToVoxelize();
+    const min = model.geometry.boundingBox.min;
+    const max = model.geometry.boundingBox.max;
+    renderVoxels(min, max, subset, 'x', 'y', 'z');
+    renderVoxels(min, max, subset, 'z', 'y', 'x');
+    renderVoxels(min, max, subset, 'x', 'z', 'y');
+}
 
-	const xIterations = getIterationNumber(voxelizedMin, voxelizedMax, 'x');
-	const yIterations = getIterationNumber(voxelizedMin, voxelizedMax, 'y');
-	const zIterations = getIterationNumber(voxelizedMin, voxelizedMax, 'z');
+function renderInnerVoxels() {
+    let previousX;
+    let previousY;
+    let previousZ;
 
-	let fillMode = false;
-	let wasLastVoxelFilled = false;
-	for(let x = 0; x < xIterations; x++) {
-		fillMode = false;
-		wasLastVoxelFilled = false;
-		for(let y = 0; y < yIterations; y++) {
-			fillMode = false;
-			wasLastVoxelFilled = false;
-			for(let z = 0; z < zIterations; z++) {
+    traverseVoxels(voxels, (index, voxel, x, y, z) => {
 
-				const xPosition = voxelizedMin.x + x * resolution;
-				const yPosition = voxelizedMin.y + y * resolution;
-				const zPosition = voxelizedMin.z + z * resolution;
+        const isStartingNewVoxelRow = previousX !== x || previousY !== y;
 
-				if(isPositionStored(xPosition, yPosition, zPosition)) {
-					if(!wasLastVoxelFilled) {
-						fillMode = !fillMode;
-					}
-					wasLastVoxelFilled = true;
-				} else {
-					if(fillMode) {
-						if(!isFillPositionStored(xPosition, yPosition, zPosition)) {
-							voxelsFillMatrices.push(new Matrix4().compose(
-								new Vector3(xPosition, yPosition, zPosition), rotation, scale,
-							));
+        if (!isStartingNewVoxelRow) {
 
-							storeFilledPosition(xPosition, yPosition, zPosition);
-						}
-					}
-					wasLastVoxelFilled = false;
-				}
-			}
-		}
-	}
+            const matrices = innerVoxels.matrices;
+            const count = (z - previousZ - resolution) / resolution;
+            for (let i = 0; i < count; i++) {
 
-	showFillVoxels();
+                innerVoxels.count++;
+                const innerZ = previousZ + i * resolution;
 
-	console.log(storedPositions);
+                if(matrices[x] === undefined) matrices[x] = {};
+                if(matrices[x][y] === undefined) matrices[x][y] = {};
 
+                matrices[x][y][innerZ] = new Matrix4().compose(
+                    new Vector3(x, y, innerZ), rotation, scale,
+                );
+
+            }
+
+        }
+
+        previousX = x;
+        previousY = y;
+        previousZ = z;
+
+    });
 }
 
 
 function showVoxels() {
-	const mesh = new InstancedMesh(geometry, material, voxelsMatrices.length);
 
-	for (let i = 0; i < voxelsMatrices.length; i++) {
-		mesh.setMatrixAt(i, voxelsMatrices[i]);
-	}
+    // const mesh = new InstancedMesh(geometry, material, voxels.count);
+    // traverseVoxels(voxels, (index, voxel) => mesh.setMatrixAt(index, voxel));
+    // scene.add(mesh);
 
-	scene.add(mesh);
+    const innerMesh = new InstancedMesh(geometry, innerMaterial, innerVoxels.count);
+    traverseVoxels(innerVoxels, (index, voxel) => innerMesh.setMatrixAt(index, voxel));
+    scene.add(innerMesh);
 
 }
 
-function showFillVoxels() {
-	const mesh = new InstancedMesh(geometry, materialFill, voxelsMatrices.length);
+function traverseVoxels(voxelObject, callback) {
+    let counter = 0;
+    for (const x in voxelObject.matrices) {
+        const voxelPlane = voxelObject.matrices[x];
+        for (const y in voxelPlane) {
+            const voxelArray = voxelPlane[y];
+            for (const z in voxelArray) {
+                const voxel = voxelArray[z];
 
-	for (let i = 0; i < voxelsFillMatrices.length; i++) {
-		mesh.setMatrixAt(i, voxelsFillMatrices[i]);
-	}
+                const xFloat = parseFloat(x);
+                const yFloat = parseFloat(y);
+                const zFloat = parseFloat(z);
 
-	scene.add(mesh);
-
+                callback(counter++, voxel, xFloat, yFloat, zFloat);
+            }
+        }
+    }
 }
 
 function renderVoxels(min, max, mesh, dimension1, dimension2, dimension3, interior = false) {
 
-	const rayCastPosition = new Vector3(min.x, min.y, min.z);
-	rayCastPosition[dimension3] -= raycastOffset;
-	const rayCastDirection = new Vector3();
-	rayCastDirection[dimension3] = 1;
+    const rayCastPosition = new Vector3(min.x, min.y, min.z);
+    rayCastPosition[dimension3] -= raycastOffset;
+    const rayCastDirection = new Vector3();
+    rayCastDirection[dimension3] = 1;
 
-	const iterations1 = getIterationNumber(min, max, dimension1);
-	const start1 = min[dimension1] + (resolution / 2);
+    const iterations1 = getIterationNumber(min, max, dimension1);
+    const start1 = min[dimension1] + (resolution / 2);
 
-	const iterations2 = getIterationNumber(min, max, dimension2);
-	const start2 = min[dimension2] + (resolution / 2);
+    const iterations2 = getIterationNumber(min, max, dimension2);
+    const start2 = min[dimension2] + (resolution / 2);
 
-	for (let i = 0; i < iterations1; i++) {
-		for (let j = 0; j < iterations2; j++) {
+    for (let i = 0; i < iterations1; i++) {
+        for (let j = 0; j < iterations2; j++) {
 
-			rayCastPosition[dimension1] = start1 + (i * resolution);
-			rayCastPosition[dimension2] = start2 + (j * resolution);
+            rayCastPosition[dimension1] = start1 + (i * resolution);
+            rayCastPosition[dimension2] = start2 + (j * resolution);
 
-			raycaster.set(rayCastPosition, rayCastDirection);
-			const results = raycaster.intersectObject(mesh);
+            raycaster.set(rayCastPosition, rayCastDirection);
+            const results = raycaster.intersectObject(mesh);
+            const matrices = voxels.matrices;
 
-			for (const result of results) {
-				const voxelizedResult = getVoxelizedPosition(result);
-				const { x, y, z } = voxelizedResult;
+            for (const result of results) {
+                const {x, y, z} = getVoxelizedPosition(result.point);
 
-				// Create voxels for limits
-				if (!isPositionStored(x, y, z)) {
-
-					voxelsMatrices.push(new Matrix4().compose(
-						new Vector3(x, y, z), rotation, scale,
-					));
-
-					storePosition(x, y, z);
-				}
-			}
-		}
-	}
+                if (matrices[x] === undefined) matrices[x] = {};
+                if (matrices[x][y] === undefined) matrices[x][y] = {};
+                if (matrices[x][y][z] === undefined) {
+                    voxels.count++;
+                    matrices[x][y][z] = new Matrix4().compose(
+                        new Vector3(x, y, z), rotation, scale,
+                    );
+                }
+            }
+        }
+    }
 }
 
 function getIterationNumber(min, max, dimension) {
-	return Math.ceil((max[dimension] - min[dimension]) / resolution);
+    return Math.ceil((max[dimension] - min[dimension]) / resolution);
 }
 
 async function getSubsetToVoxelize() {
-	const wallsStandardIDs = await ifcLoader.ifcManager.getAllItemsOfType(0, IFCWALLSTANDARDCASE, false);
-	const wallsIDs = await ifcLoader.ifcManager.getAllItemsOfType(0, IFCWALL, false);
-	const windowsIDs = await ifcLoader.ifcManager.getAllItemsOfType(0, IFCWINDOW, false);
-	const slabsIDs = await ifcLoader.ifcManager.getAllItemsOfType(0, IFCSLAB, false);
-	const membersIDs = await ifcLoader.ifcManager.getAllItemsOfType(0, IFCMEMBER, false);
-	const platesIDs = await ifcLoader.ifcManager.getAllItemsOfType(0, IFCPLATE, false);
-	const doorsIDs = await ifcLoader.ifcManager.getAllItemsOfType(0, IFCDOOR, false);
-	const ids = [...wallsIDs, ...wallsStandardIDs, ...windowsIDs, ...membersIDs, ...platesIDs, ...doorsIDs, ...slabsIDs];
+    const wallsStandardIDs = await ifcLoader.ifcManager.getAllItemsOfType(0, IFCWALLSTANDARDCASE, false);
+    const wallsIDs = await ifcLoader.ifcManager.getAllItemsOfType(0, IFCWALL, false);
+    const windowsIDs = await ifcLoader.ifcManager.getAllItemsOfType(0, IFCWINDOW, false);
+    const slabsIDs = await ifcLoader.ifcManager.getAllItemsOfType(0, IFCSLAB, false);
+    const membersIDs = await ifcLoader.ifcManager.getAllItemsOfType(0, IFCMEMBER, false);
+    const platesIDs = await ifcLoader.ifcManager.getAllItemsOfType(0, IFCPLATE, false);
+    const doorsIDs = await ifcLoader.ifcManager.getAllItemsOfType(0, IFCDOOR, false);
+    const ids = [...wallsIDs, ...wallsStandardIDs, ...windowsIDs, ...membersIDs, ...platesIDs, ...doorsIDs, ...slabsIDs];
 
-	return ifcLoader.ifcManager.createSubset({
-		modelID: 0,
-		ids,
-		applyBVH: true,
-		removePrevious: true,
-	});
+    return ifcLoader.ifcManager.createSubset({
+        modelID: 0,
+        ids,
+        applyBVH: true,
+        removePrevious: true,
+    });
 }
 
-function getVoxelizedPosition(result) {
-	const x = Math.trunc(result.point.x / resolution) * resolution;
-	const y = Math.trunc(result.point.y / resolution) * resolution;
-	const z = Math.trunc(result.point.z / resolution) * resolution;
-	return { x, y, z };
-}
-
-function isPositionStored(x, y, z) {
-	return storedPositions[x] !== undefined && storedPositions[x][y] !== undefined && storedPositions[x][y][z] !== undefined;
-}
-
-function isFillPositionStored(x, y, z) {
-	return storedFilledPositions[x] !== undefined && storedFilledPositions[x][y] !== undefined && storedFilledPositions[x][y][z] !== undefined;
-}
-
-function storePosition(x, y, z) {
-	if (!storedPositions[x]) {
-		storedPositions[x] = { x };
-	}
-
-	if (!storedPositions[x][y]) {
-		storedPositions[x][y] = { y };
-	}
-
-	storedPositions[x][y][z] = z;
-}
-
-function storeFilledPosition(x, y, z) {
-	if (!storedFilledPositions[x]) {
-		storedFilledPositions[x] = { x };
-	}
-
-	if (!storedFilledPositions[x][y]) {
-		storedFilledPositions[x][y] = { y };
-	}
-
-	storedFilledPositions[x][y][z] = z;
+function getVoxelizedPosition(point) {
+    const x = Math.trunc(point.x / resolution) * resolution;
+    const y = Math.trunc(point.y / resolution) * resolution;
+    const z = Math.trunc(point.z / resolution) * resolution;
+    return {x, y, z};
 }
 
 loadIFC();
