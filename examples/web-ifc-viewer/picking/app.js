f8c0dd16da9502a0f03e169e522187567ae228e0
seantai <seantai@protonmail.com>
2022-06-14 16:34:58 +0200

    update with picking methods

--- examples/web-ifc-viewer/picking/app.js
+++ examples/web-ifc-viewer/picking/app.js
@@ -1,18 +1,33 @@
 import { IfcViewerAPI } from 'web-ifc-viewer';
 
 const container = document.getElementById('viewer-container');
-const viewer = new IfcViewerAPI({ container });
-viewer.IFC.setWasmPath('../../../');
-viewer.IFC.loadIfcUrl('../../../IFC/01.ifc');
+const viewer = new IfcViewerAPI({ container});
 
-function pick() {
-	const result = viewer.context.castRayIfc();
-	console.log(result);
-	if (!result) return;
-	const manager = viewer.IFC.loader.ifcManager;
-	const id = manager.getExpressId(result.object.geometry, result.faceIndex);
-    output.innerHTML = id;
+async function loadIfc(url) {
+    await viewer.IFC.setWasmPath("../../../");
+    const model = await viewer.IFC.loadIfcUrl(url);
+    viewer.shadowDropper.renderShadow(model.modelID);
 }
-const output = document.getElementById("id-output");
+loadIfc('../../../IFC/01.ifc');
 
-window.ondblclick = () => pick(viewer);
+window.onmousemove = () => viewer.IFC.selector.prePickIfcItem(true);
+
+window.onclick = async () => {
+    const {modelID, id} = await viewer.IFC.selector.pickIfcItem(true);
+    const props = await viewer.IFC.getProperties(modelID, id, true, false);
+    console.log(props);
+}
+
+window.ondblclick = () => viewer.IFC.selector.highlightIfcItem();
+
+window.onkeydown = (event) => {
+    if(event.code === 'KeyC') {
+        viewer.IFC.selector.unpickIfcItems();
+        viewer.IFC.selector.unHighlightIfcItems();
+    }
+}
+
+document.getElementById('express_22492')
+.addEventListener('click', () => {
+    viewer.IFC.selector.pickIfcItemsByID(0, [22492], true);
+})
