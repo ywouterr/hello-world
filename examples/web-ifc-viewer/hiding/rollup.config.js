e41f8a7d731a39627c83daf8203cb6aa6cc8bb54
Antonio González Viegas <antoniogviegas@hotmail.com>
2022-06-17 00:17:53 +0200

    Add web-ifc-viewer hiding example

---
+++ examples/web-ifc-viewer/hiding/rollup.config.js
@@ -0,0 +1,14 @@
+import resolve from '@rollup/plugin-node-resolve';
+
+export default {
+  input: 'examples/web-ifc-viewer/hiding/app.js',
+  output: [
+    {
+      format: 'esm',
+      file: 'examples/web-ifc-viewer/hiding/bundle.js'
+    },
+  ],
+  plugins: [
+    resolve(),
+  ]
+};
