5d8f9fb7d613c13c912d288c9006b798bba7959a
Antonio González Viegas <antoniogviegas@hotmail.com>
2022-06-22 00:39:02 +0200

    Add psets to gltf import example

--- examples/web-ifc-viewer/gltf-import/bundle.js
+++ examples/web-ifc-viewer/gltf-import/bundle.js
@@ -113742,6 +113742,7 @@ load();
 window.ondblclick = async () => {
 	const result = await viewer.IFC.selector.pickIfcItem(true);
 	const foundProperties = properties[result.id];
+	getPropertySets(foundProperties);
 	console.log(foundProperties);
 };
 window.onmousemove = () => viewer.IFC.selector.prePickIfcItem();
@@ -113814,4 +113815,18 @@ async function constructSpatialTreeNode(
 	}
 
 	item.children = children;
+}
+
+// Gets the property sets
+
+function getPropertySets(props) {
+	const id = props.expressID;
+	const propertyValues = Object.values(properties);
+	const allPsetsRels = propertyValues.filter(item => item.type === 'IFCRELDEFINESBYPROPERTIES');
+	const relatedPsetsRels = allPsetsRels.filter(item => item.RelatedObjects.includes(id));
+	const psets = relatedPsetsRels.map(item => properties[item.RelatingPropertyDefinition]);
+	for(let pset of psets) {
+		pset.HasProperty = pset.HasProperties.map(id => properties[id]);
+	}
+	props.psets = psets;
 }
